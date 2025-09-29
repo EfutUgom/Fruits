98a49d17967454ea346b07fd8540d3c6fd43f187
EfutUgom <97000184+EfutUgom@users.noreply.github.com>
2025-09-29 18:28:01 +0100

    Add files via upload

---
+++ fruits.js
@@ -0,0 +1,28 @@
+const current = document.querySelector('#current');
+const imgs = document.querySelectorAll('.imgs, img');
+const opacity = 1;
+
+// imgs[1].style.opacity = opacity;
+
+imgs.forEach(img =>
+	img.addEventListener('click', e =>
+		(current.src=e.target.src))
+	);
+
+img.forEach(img =>
+	img.addEventListener('click', imgClick));
+
+function imgClick (e){
+	imgs.forEach(img=>
+		(img.style.opacity=1));
+
+	current.src = e.target.src;
+
+	current.classList.add('fade-in');
+
+	setTimeout(() =>
+		current.classList.remove('fade-in'), 
+		500);
+
+	e.target.style.opacity=opacity;
+};
